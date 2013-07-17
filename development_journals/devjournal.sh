fda9691eb380dc0b5ae26273549b637459e87dde
maaron34 <michaeltaaron@gmail.com>
2013-07-16 23:14:27 -0400

    Add development journal

---
+++ development_journals/devjournal.sh
@@ -0,0 +1,9 @@
+echo $1
+echo $2
+
+mkdir development_journals
+cd development_journals
+
+echo "http://developmentjournal.tumblr.com/" >> aaron_michael.txt
+cd
+cat code/bewd/BEWDiful_Students/development_journals/aaron_michael.txt
---
+++ devjournal.sh
@@ -0,0 +1,7 @@
+
+mkdir development_journals
+cd development_journals
+
+echo "http://developmentjournal.tumblr.com/" >> aaron_michael.txt
+cd
+cat code/bewd/BEWDiful_Students/development_journals/aaron_michael.txt
